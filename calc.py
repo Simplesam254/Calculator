9714a24e508de8d89d32cfd17c5b6af80b42623c
Simplesam254 <samuelokello690@gmail.com>
2025-08-12 18:43:59 +0300

    first commit

---
+++ calc.py
@@ -0,0 +1,13 @@
+num1 =float(input('Enter your first number: '))
+num2 =float(input('Enter your second number: '))
+
+sum = num1 + num2
+subtraction = num1 - num2
+division = num1/num2
+multiplication = num1 * num2
+
+print("\n---Results---")
+print(f"Sum: {sum}")
+print(f"Difference: {subtraction}")
+print(f"Division: {division}")
+print(f"Product: {multiplication}")
